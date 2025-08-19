f1d6485169bc362462520f3ea3b11cd5f1e08da1
Paul Tibbetts <code@paultibbetts.uk>
2025-08-19 13:03:34 +0100

    chore: fix an example

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     mythicbeasts = {
       source  = "paultibbetts.uk/terraform-providers/mythicbeasts"
@@ -11,3 +13,6 @@ provider "mythicbeasts" {}
 
 data "mythicbeasts_pis" "example" {}
 
+output "example" {
+  value = data.mythicbeasts_pis.example
+}
