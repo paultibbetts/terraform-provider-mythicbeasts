38f9c779695f28f16509705a3d2fc3adb1235928
Paul Tibbetts <code@paultibbetts.uk>
2025-08-20 13:05:21 +0100

    test: add vps pricing tests

---
+++ internal/provider/vps_pricing_data_source_test.go
@@ -0,0 +1,47 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+func TestAccVPSPricingDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Read testing
+			{
+				Config: testAccVPSPricingDataSourceConfig,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"data.mythicbeasts_vps_pricing.all",
+						tfjsonpath.New("ipv4"),
+						knownvalue.NotNull(),
+					),
+					statecheck.ExpectKnownValue(
+						"data.mythicbeasts_vps_pricing.all",
+						tfjsonpath.New("disk"),
+						knownvalue.ObjectPartial(map[string]knownvalue.Check{
+							"ssd": knownvalue.ObjectPartial(map[string]knownvalue.Check{
+								"price":  knownvalue.NotNull(),
+								"extent": knownvalue.NotNull(),
+							}),
+						}),
+					),
+				},
+			},
+		},
+	})
+}
+
+const testAccVPSPricingDataSourceConfig = `
+data "mythicbeasts_vps_pricing" "all" {}
+`
